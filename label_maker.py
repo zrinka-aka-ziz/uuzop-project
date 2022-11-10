949cb28209763263e014cb59c14014bd1cb44328
zrinka-aka-ziz <zpecanic@gmail.com>
2022-11-10 03:43:22 +0100

    Updated data and label_maker.py

--- label_maker.py
+++ label_maker.py
@@ -1,3 +1,7 @@
+#import pandas as pd
+#import csv
+
+
 labels='Age,Sex,Height,Weight,QRSduration,PRinterval,QTinterval,Tinterval,Pinterval,QRSangle,Tangle,Pangle,QRSTangle,Jangle,HeartRate,'
 group1 = ['Qwidth','Rwidth','Swidth','SmallRwidth','SmallSwidth','IntDeflNum','RaggedR','DiphasicR','RaggedP','DiphasicP','RaggedT','DiphasicT']
 channels = ['DI', 'DII', 'DIII', 'AVR', 'AVL', 'AVF', 'V1','V2','V3','V4','V5','V6']
@@ -16,13 +20,19 @@ for channel in channels:
     for col in group2:
         new_col= channel+col+',' #new_col= channel+'_'+col+',' ako zelimo underscore da odvaja kanale i ono sto je mjereno
         labels+=new_col
+labels+="Class" #zadnji stupac je klasa dodijeljena svakoj osobi
 
-labels1 = labels[:-1] #makni zadnji zarez
-labels_list=labels1.split(',')
+labels_list=labels.split(',')
 print(len(labels_list))
-
+print(labels_list)
+#n=1
 #for el in labels_list:
-    #print(el)
+    #print(str(n)+" "+el)
+    #n+=1
     
-u_labels=",".join(labels_list)
+#u_labels=",".join(labels_list)
 #print(u_labels)
+
+
+
+
